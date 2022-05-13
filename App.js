25a9cd24150f036a546534ca3fdaaaff6f61359c
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-05-13 16:37:21 -0500

    Add form to modal and style inputs

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Alert, Modal, StyleSheet, Text, Pressable, View, Image, TextInput } from "react-native";
-import MyCarousel from "./MyCarousel";
+import { withTheme } from "styled-components";
+import MyCarousel from "./Components/MyCarousel";
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -9,13 +11,7 @@ const App = () => {
       <Image source={require('./plant-connect-icon.png')} style={styles.tinyLogo} />
       <Text>Planty Dropper</Text>
       <MyCarousel />
-      <TextInput
-        style={styles.input}
-        // onChangeText={onChangeText}
-        placeholder="Plant Name"
-        // value={text}
-      />
-    
+      
       <Modal
         animationType="slide"
         transparent={true}
@@ -27,12 +23,36 @@ const App = () => {
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
+            <Text style={styles.modalText}>Please Fill Out Form</Text>
+            <DropDownPicker style={styles.dropdown} placeholder="Category"
+    items={[
+        {label: 'Item 1', value: 'item1'},
+        {label: 'Item 2', value: 'item2'},
+    ]}
+    defaultIndex={0}
+    containerStyle={{height: 40}}
+    onChangeItem={item => console.log(item.label, item.value)}
+/>
+            <TextInput style={styles.input} 
+      // onChangeText={onChangeText}
+        placeholder="Plant Name"
+        // value={text}
+      />
+      <TextInput style={styles.input} 
+      // onChangeText={onChangeText}
+        placeholder="Plant Name"
+        // value={text}
+      />
+      <TextInput style={styles.input} 
+      // onChangeText={onChangeText}
+        placeholder="Plant Name"
+        // value={text}
+      />
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => setModalVisible(!modalVisible)}
             >
-              <Text style={styles.textStyle}>Hide Modal</Text>
+              <Text style={styles.textStyle}>Submit</Text>
             </Pressable>
           </View>
         </View>
@@ -41,7 +61,7 @@ const App = () => {
         style={[styles.button, styles.buttonOpen]}
         onPress={() => setModalVisible(true)}
       >
-        <Text style={styles.textStyle}>Show Modal</Text>
+        <Text style={styles.textStyle}>Post Your Plant!</Text>
       </Pressable>
     </View>
   );
@@ -54,16 +74,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 22
   },
+  dropdown: {
+    height: 20,
+    width: 160,
+    borderWidth: 1,
+  },
   modalView: {
     margin: 20,
+    height: 400,
     backgroundColor: "green",
     borderRadius: 20,
+    alignContent: 'center',
+    justifyContent: "space-between",
     padding: 35,
     alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
-      width: 0,
-      height: 2
+      width: 10,
+      height: 10
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
@@ -78,16 +106,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#F194FF",
   },
   buttonClose: {
-    backgroundColor: "#2196F3",
+    backgroundColor: "#F194FF",
+    width: 160,
   },
   textStyle: {
     color: "white",
     fontWeight: "bold",
     textAlign: "center",
-    marginBottom: 15,
+    // marginBottom: 15,
   },
   modalText: {
-    marginBottom: 15,
     textAlign: "center"
   },
   input: {
@@ -102,6 +130,12 @@ const styles = StyleSheet.create({
     alignSelf: "flex-start",
     marginLeft: 20,
   },
+  input: {
+    backgroundColor: 'white',
+    width: 160,
+    height: 40,
+    margin: 1,
+  }
 });
 
 export default App;
