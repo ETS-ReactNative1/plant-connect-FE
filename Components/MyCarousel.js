2609f1d84e1b0d22436f2639bc2a8acd6304b793
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-05-14 18:01:48 -0600

    Convert MyCarousel component to hooks

--- Components/MyCarousel.js
+++ Components/MyCarousel.js
@@ -1,88 +1,88 @@
 import * as React from 'react'
+import { useState } from 'react'
 import { Text, View, StyleSheet, Pressable, SafeAreaView } from 'react-native'
 
-import Carousel from 'react-native-snap-carousel'
+import styled from 'styled-components'
 
-export default class MyCarousel extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			activeIndex: 0,
-			plants: [
-				{
-					title: 'Monsterra',
-					text: 'Text 1',
-				},
-				{
-					title: 'Snake Plant',
-					text: 'Text 2',
-				},
-				{
-					title: 'Swiss Cheese Plant',
-					text: 'Text 3',
-				},
-				{
-					title: 'Egg Plant',
-					text: 'Text 4',
-				},
-				{
-					title: 'Banana Ficus',
-					text: 'Text 5',
-				},
-			],
-			seeds: [
-				{
-					title: 'Papaya',
-					text: 'Text 1',
-				},
-				{
-					title: 'Tomato',
-					text: 'Text 2',
-				},
-				{
-					title: 'Apple',
-					text: 'Text 3',
-				},
-				{
-					title: 'Poppy',
-					text: 'Text 4',
-				},
+import Carousel, { ParallaxImage } from 'react-native-snap-carousel'
 
-				{
-					title: 'Pumpkin',
-					text: 'Text 5',
-				},
-			],
-			clippings: [
-				{
-					title: 'Spider',
-					text: 'Text 1',
-				},
-				{
-					title: 'Ivy',
-					text: 'Text 2',
-				},
-				{
-					title: 'Other',
-					text: 'Text 3',
-				},
-				{
-					title: 'Another other',
-					text: 'Text 4',
-				},
-				{
-					title: 'Potato',
-					text: 'Text 5',
-				},
-			],
-			modalVisible: false,
-		}
+const MyCarousel = () => {
+	const [activeIndex, setActiveIndex] = useState(0)
+	const [plants, setPlants] = useState([
+		{
+			title: 'Monsterra',
+			text: 'Text 1',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/1/1c/Monstera_Adansonii.jpg',
+		},
+		{
+			title: 'Snake Plant',
+			text: 'Text 2',
+		},
+		{
+			title: 'Swiss Cheese Plant',
+			text: 'Text 3',
+		},
+		{
+			title: 'Egg Plant',
+			text: 'Text 4',
+		},
+		{
+			title: 'Banana Ficus',
+			text: 'Text 5',
+		},
+	])
+	const [clippings, setClippings] = useState([
+		{
+			title: 'Spider',
+			text: 'Text 1',
+		},
+		{
+			title: 'Ivy',
+			text: 'Text 2',
+		},
+		{
+			title: 'Other',
+			text: 'Text 3',
+		},
+		{
+			title: 'Another other',
+			text: 'Text 4',
+		},
+		{
+			title: 'Potato',
+			text: 'Text 5',
+		},
+	])
+	const [seeds, setSeeds] = useState([
+		{
+			title: 'Papaya',
+			text: 'Text 1',
+		},
+		{
+			title: 'Tomato',
+			text: 'Text 2',
+		},
+		{
+			title: 'Apple',
+			text: 'Text 3',
+		},
+		{
+			title: 'Poppy',
+			text: 'Text 4',
+		},
+
+		{
+			title: 'Pumpkin',
+			text: 'Text 5',
+		},
+	])
+	const [modalVisible, setModalVisible] = useState(false)
+
+	const hideModal = () => {
+		setModalVisible(false)
 	}
-	hideModal() {
-		this.setState({ modalVisible: false })
-	}
-	_renderItem({ item, index }) {
-		// const [modalVisible, setModalVisible] = useState(false);
+	const _renderItem = ({ item, index, parallaxProps }) => {
 		return (
 			<View
 				style={{
@@ -97,7 +97,11 @@ export default class MyCarousel extends React.Component {
 				}}>
 				<Text style={{ fontSize: 20 }}>{item.title}</Text>
 				<Text>{item.text}</Text>
-
+				<ParallaxImage
+					source={{
+						uri: `https://upload.wikimedia.org/wikipedia/commons/1/1c/Monstera_Adansonii.jpg`,
+					}}
+				/>
 				<Pressable
 					style={[styles.button, styles.buttonOpen]}
 					// onPress={() => this.setState({modalVisible: true})}
@@ -108,55 +112,54 @@ export default class MyCarousel extends React.Component {
 		)
 	}
 
-	render() {
-		return (
-			<SafeAreaView
-				style={{
-					flex: 4,
-					backgroundColor: '#57784E',
-					paddingTop: 1,
-					borderRadius: 12,
-					height: 500,
-				}}>
-				<View
-					style={{ flex: 0.5, flexDirection: 'row', justifyContent: 'center' }}>
-					<Carousel
-						layout={'default'}
-						ref={(ref) => (this.carousel = ref)}
-						data={this.state.plants}
-						sliderWidth={100}
-						itemWidth={120}
-						renderItem={this._renderItem}
-						onSnapToItem={(index) => this.setState({ activeIndex: index })}
-					/>
-				</View>
-				<View
-					style={{ flex: 0.5, flexDirection: 'row', justifyContent: 'center' }}>
-					<Carousel
-						layout={'default'}
-						ref={(ref) => (this.carousel = ref)}
-						data={this.state.seeds}
-						sliderWidth={300}
-						itemWidth={300}
-						renderItem={this._renderItem}
-						onSnapToItem={(index) => this.setState({ activeIndex: index })}
-					/>
-				</View>
-				<View
-					style={{ flex: 0.4, flexDirection: 'row', justifyContent: 'center' }}>
-					<Carousel
-						layout={'default'}
-						ref={(ref) => (this.carousel = ref)}
-						data={this.state.clippings}
-						sliderWidth={300}
-						itemWidth={300}
-						renderItem={this._renderItem}
-						onSnapToItem={(index) => this.setState({ activeIndex: index })}
-					/>
-				</View>
-			</SafeAreaView>
-		)
-	}
+	return (
+		<SafeAreaView
+			style={{
+				flex: 4,
+				backgroundColor: '#57784E',
+				paddingTop: 1,
+				borderRadius: 12,
+				height: 500,
+			}}>
+			<View
+				style={{ flex: 0.5, flexDirection: 'row', justifyContent: 'center' }}>
+				<Carousel
+					layout={'default'}
+					ref={(ref) => (this.carousel = ref)}
+					data={plants}
+					sliderWidth={100}
+					itemWidth={120}
+					renderItem={_renderItem}
+					onSnapToItem={(index) => setActiveIndex(index)}
+					hasParallaxImages={true}
+				/>
+			</View>
+			<View
+				style={{ flex: 0.5, flexDirection: 'row', justifyContent: 'center' }}>
+				<Carousel
+					layout={'default'}
+					ref={(ref) => (this.carousel = ref)}
+					data={seeds}
+					sliderWidth={300}
+					itemWidth={300}
+					renderItem={_renderItem}
+					onSnapToItem={(index) => setActiveIndex(index)}
+				/>
+			</View>
+			<View
+				style={{ flex: 0.4, flexDirection: 'row', justifyContent: 'center' }}>
+				<Carousel
+					layout={'default'}
+					ref={(ref) => (this.carousel = ref)}
+					data={clippings}
+					sliderWidth={300}
+					itemWidth={300}
+					renderItem={_renderItem}
+					onSnapToItem={(index) => setActiveIndex(index)}
+				/>
+			</View>
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
@@ -173,4 +176,13 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		//   marginBottom: 15,
 	},
+	imageStyle: {
+		height: 100,
+	},
 })
+
+const cardBackground = styled.ImageBackground`
+	width: 100%;
+	height: 100%;
+`
+export default MyCarousel
