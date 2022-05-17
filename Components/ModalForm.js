80a3468f8fea87ae335c4dfce04062fafb9f38a7
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-05-17 14:25:16 -0600

    Test commit

--- Components/ModalForm.js
+++ Components/ModalForm.js
@@ -1,214 +1,206 @@
-import React, { useState } from "react";
-import DropDownPicker from "react-native-dropdown-picker";
-import CameraView from "./CameraView";
+import React, { useState } from 'react'
+import DropDownPicker from 'react-native-dropdown-picker'
+import CameraView from './CameraView'
 import {
-  Modal,
-  StyleSheet,
-  Text,
-  Pressable,
-  View,
-  TextInput,
-} from "react-native";
-import CameraModal from "./CameraModal";
-import { CheckBox } from 'react-native-elements';
-import RadioButtonRN from 'radio-buttons-react-native';
-
+	Modal,
+	StyleSheet,
+	Text,
+	Pressable,
+	View,
+	TextInput,
+} from 'react-native'
+import CameraModal from './CameraModal'
+import { CheckBox } from 'react-native-elements'
+import RadioButtonRN from 'radio-buttons-react-native'
+// HELLO BRENDA
 export default function ModalForm({
-  visible,
-  setModalVisible,
-  createNewListing,
-  cameraModalVisible,
-  setCameraModalVisible,
+	visible,
+	setModalVisible,
+	createNewListing,
+	cameraModalVisible,
+	setCameraModalVisible,
 }) {
-  const [plantName, setPlantName] = useState("");
-  const [description, setDescription] = useState("");
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
-  const [quantity, setQuantity] = useState(0);
-  const [currentListing, setCurrentListing] = useState("");
-  const [items, setItems] = useState([
-    { label: "plant", value: "plant" },
-    { label: "clipping", value: "clipping" },
-    { label: "seeds", value: "seeds" },
-  ]);
-  const [rooted, setRooted] = useState(false);
-  const [unRooted, setUnRooted] = useState(false);
-  const [indoor, setIndoor] = useState(false);
+	const [plantName, setPlantName] = useState('')
+	const [description, setDescription] = useState('')
+	const [open, setOpen] = useState(false)
+	const [value, setValue] = useState(null)
+	const [quantity, setQuantity] = useState(0)
+	const [currentListing, setCurrentListing] = useState('')
+	const [items, setItems] = useState([
+		{ label: 'plant', value: 'plant' },
+		{ label: 'clipping', value: 'clipping' },
+		{ label: 'seeds', value: 'seeds' },
+	])
+	const [rooted, setRooted] = useState(false)
+	const [unRooted, setUnRooted] = useState(false)
+	const [indoor, setIndoor] = useState(false)
 
-  const submitListing = () => {
-    setModalVisible(false);
-    const newListing = {
-      listingID: Date.now(),
-      plantName: plantName,
-      category: value,
-      description: description,
-      quantity: quantity,
-    };
-    createNewListing(newListing);
-    clearInputs();
-  };
-  const clearInputs = () => {
-    setDescription("");
-    setQuantity(0);
-    setPlantName("");
-  };
+	const submitListing = () => {
+		setModalVisible(false)
+		const newListing = {
+			listingID: Date.now(),
+			plantName: plantName,
+			category: value,
+			description: description,
+			quantity: quantity,
+		}
+		createNewListing(newListing)
+		clearInputs()
+	}
+	const clearInputs = () => {
+		setDescription('')
+		setQuantity(0)
+		setPlantName('')
+	}
 
-const buttonList = [
-  {label: 'rooted'},
-  {label: 'unRooted'}
-]
+	const buttonList = [{ label: 'rooted' }, { label: 'unRooted' }]
 
-  const setRootedState = (status) => {
-    status ? `set${{status}}`(false) : `set${status}`(true)
-  }
+	const setRootedState = (status) => {
+		status ? `set${{ status }}`(false) : `set${status}`(true)
+	}
 
-  return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={visible}
-      onRequestClose={() => {
-        Alert.alert("Modal has been closed.");
-        setModalVisible(false);
-      }}
-    >
-      <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-          <Text style={styles.modalText}>Post a Plant!</Text>
-          <Text>name: {plantName}</Text>
-          <Text>description: {description}</Text>
-          <Text>quantity: {quantity}</Text>
-          <Text>rooted: {rooted}</Text>
-          <DropDownPicker
-            open={open}
-            style={styles.dropdown}
-            value={value}
-            placeholder="Category"
-            defaultIndex={0}
-            containerStyle={{ height: 40 }}
-            items={items}
-            setOpen={setOpen}
-            setValue={setValue}
-            setItems={setItems}
-          />
-          <View>
-            <RadioButtonRN
-              name='Rooted'
-              data={buttonList}
-             />
-          </View>
-          <TextInput
-            style={styles.input}
-            onChangeText={setPlantName}
-            placeholder="Plant Name"
-            value={plantName}
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={setDescription}
-            placeholder="Description"
-            value={description}
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={setQuantity}
-            placeholder="Quantity"
-            value={quantity}
-          />
-          <Pressable
-            style={[styles.button, styles.buttonOpen]}
-            onPress={() => setCameraModalVisible(true)}
-          >
-            <Text style={styles.textStyle}>Take Picture</Text>
-          </Pressable>
-		  {cameraModalVisible && <CameraModal setCameraModalVisible={setCameraModalVisible} />}
-          <Pressable
-            style={[styles.button, styles.buttonClose]}
-            onPress={() => submitListing()}
-          >
-            <Text style={styles.textStyle}>Submit</Text>
-          </Pressable>
-        </View>
-      </View>
-    </Modal>
-  );
+	return (
+		<Modal
+			animationType='slide'
+			transparent={true}
+			visible={visible}
+			onRequestClose={() => {
+				Alert.alert('Modal has been closed.')
+				setModalVisible(false)
+			}}>
+			<View style={styles.centeredView}>
+				<View style={styles.modalView}>
+					<Text style={styles.modalText}>Post a Plant!</Text>
+					<Text>name: {plantName}</Text>
+					<Text>description: {description}</Text>
+					<Text>quantity: {quantity}</Text>
+					<Text>rooted: {rooted}</Text>
+					<DropDownPicker
+						open={open}
+						style={styles.dropdown}
+						value={value}
+						placeholder='Category'
+						defaultIndex={0}
+						containerStyle={{ height: 40 }}
+						items={items}
+						setOpen={setOpen}
+						setValue={setValue}
+						setItems={setItems}
+					/>
+					<View>
+						<RadioButtonRN name='Rooted' data={buttonList} />
+					</View>
+					<TextInput
+						style={styles.input}
+						onChangeText={setPlantName}
+						placeholder='Plant Name'
+						value={plantName}
+					/>
+					<TextInput
+						style={styles.input}
+						onChangeText={setDescription}
+						placeholder='Description'
+						value={description}
+					/>
+					<TextInput
+						style={styles.input}
+						onChangeText={setQuantity}
+						placeholder='Quantity'
+						value={quantity}
+					/>
+					<Pressable
+						style={[styles.button, styles.buttonOpen]}
+						onPress={() => setCameraModalVisible(true)}>
+						<Text style={styles.textStyle}>Take Picture</Text>
+					</Pressable>
+					{cameraModalVisible && (
+						<CameraModal setCameraModalVisible={setCameraModalVisible} />
+					)}
+					<Pressable
+						style={[styles.button, styles.buttonClose]}
+						onPress={() => submitListing()}>
+						<Text style={styles.textStyle}>Submit</Text>
+					</Pressable>
+				</View>
+			</View>
+		</Modal>
+	)
 }
 
 const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  dropdown: {
-    height: 20,
-    width: 160,
-    marginLeft: 31,
-    borderWidth: 1,
-  },
-  modalView: {
-    margin: 20,
-    height: 500,
-    width: 300,
-    backgroundColor: "#FFF9EB",
-    borderRadius: 20,
-    alignContent: "center",
-    justifyContent: "space-between",
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 15,
-      height: 15,
-    },
-    shadowOpacity: 0.7,
-    shadowRadius: 5,
-    elevation: 9,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#545454",
-  },
-  buttonClose: {
-    backgroundColor: "#545454",
-    width: 160,
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-    // marginBottom: 15,
-  },
-  modalText: {
-    textAlign: "center",
-  },
-  input: {
-    height: 40,
-    // margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-  input: {
-    backgroundColor: "#C4C4C4",
-    width: 160,
-    height: 40,
-    margin: 1,
-  },
-  radioContainer: {
-    border: 1,
-    borderColor: 'red',
-    borderWidth: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    
-  },
-  radio: {
-   buttonColor: 'black',
-  //  color: 'red'
-  }
-});
+	centeredView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 22,
+	},
+	dropdown: {
+		height: 20,
+		width: 160,
+		marginLeft: 31,
+		borderWidth: 1,
+	},
+	modalView: {
+		margin: 20,
+		height: 500,
+		width: 300,
+		backgroundColor: '#FFF9EB',
+		borderRadius: 20,
+		alignContent: 'center',
+		justifyContent: 'space-between',
+		padding: 35,
+		alignItems: 'center',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 15,
+			height: 15,
+		},
+		shadowOpacity: 0.7,
+		shadowRadius: 5,
+		elevation: 9,
+	},
+	button: {
+		borderRadius: 20,
+		padding: 10,
+		elevation: 2,
+	},
+	buttonOpen: {
+		backgroundColor: '#545454',
+	},
+	buttonClose: {
+		backgroundColor: '#545454',
+		width: 160,
+	},
+	textStyle: {
+		color: 'white',
+		fontWeight: 'bold',
+		textAlign: 'center',
+		// marginBottom: 15,
+	},
+	modalText: {
+		textAlign: 'center',
+	},
+	input: {
+		height: 40,
+		// margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
+	input: {
+		backgroundColor: '#C4C4C4',
+		width: 160,
+		height: 40,
+		margin: 1,
+	},
+	radioContainer: {
+		border: 1,
+		borderColor: 'red',
+		borderWidth: 10,
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	radio: {
+		buttonColor: 'black',
+		//  color: 'red'
+	},
+})
