82a0c6ba5d4c10bd02af05cbd12c95f023642974
Emily Corcoran <cathleencorcoran@gmail.com>
2022-05-22 18:42:13 -0400

    Add cypress test for mainPageView. Not currently running.

--- cypress/integration/mainView.js
+++ cypress/integration/mainView.js
@@ -1,6 +1,10 @@
 describe('Main page flow', () => {
 
-    it('should allow user to search for an artist', () => {
-      cy.visit('http://localhost:19000/')
-    });
-  })
+    it('should allow user to visit the main page and see a title', () => {
+      cy.visit("http://localhost:19003")
+      .contains("planty dropper")
+      
+    })
+
+
+  });
