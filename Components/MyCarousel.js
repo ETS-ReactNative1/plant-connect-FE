a1091dbb90e3abbe09eebcba8a4f23d273bd7e1d
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-05-21 18:14:40 -0600

    Add message modal

--- Components/MyCarousel.js
+++ Components/MyCarousel.js
@@ -1,177 +1,166 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import { listings, getData } from "../apiCalls";
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import { listings, getData } from '../apiCalls'
 import {
-  Text,
-  View,
-  StyleSheet,
-  Pressable,
-  SafeAreaView,
-  Image,
-  ImageBackground,
-} from "react-native";
+	Text,
+	View,
+	StyleSheet,
+	Pressable,
+	SafeAreaView,
+	Image,
+	ImageBackground,
+} from 'react-native'
 
+import Carousel from 'react-native-snap-carousel'
 
-import Carousel from "react-native-snap-carousel";
+const MyCarousel = ({ setPlantModalVisible, setCurrentListing }) => {
+	const [activeIndex, setActiveIndex] = useState(0)
+	const [plants, setPlants] = useState([])
+	const [clippings, setClippings] = useState([])
+	const [seeds, setSeeds] = useState([])
+	//   const [modalVisible, setModalVisible] = useState(false);
 
-const MyCarousel = ({setPlantModalVisible, setCurrentListing}) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [plants, setPlants] = useState([]);
-  const [clippings, setClippings] = useState([]);
-  const [seeds, setSeeds] = useState([]);
-//   const [modalVisible, setModalVisible] = useState(false);  
+	useEffect(() => {
+		getData().then((data) => {
+			setCategories(data.data.attributes)
+		})
+	}, [])
 
-  useEffect(() => {
-    getData().then((data) => {
-      setCategories(data.data.attributes);
-    });
-  }, []);
+	const setCategories = (data) => {
+		const filteredPlants = data.filter(
+			(listing) => listing.category === 'plant'
+		)
+		const filteredSeeds = data.filter((listing) => listing.category === 'seeds')
+		const filteredClippings = data.filter(
+			(listing) => listing.category === 'clippings'
+		)
 
-  const setCategories = (data) => {
-    const filteredPlants = data.filter(
-      (listing) => listing.category === "plant"
-    );
-    const filteredSeeds = data.filter(
-      (listing) => listing.category === "seeds"
-    );
-    const filteredClippings = data.filter(
-      (listing) => listing.category === "clippings"
-    );
+		setPlants(filteredPlants)
+		setSeeds(filteredSeeds)
+		setClippings(filteredClippings)
+	}
 
-    setPlants(filteredPlants);
-    setSeeds(filteredSeeds);
-    setClippings(filteredClippings);
-  };
+	const showPlantModal = (item) => {
+		setCurrentListing(item)
+		setPlantModalVisible(true)
+	}
 
-  const showPlantModal = (item) => {
-	  
-	setCurrentListing(item)
-    setPlantModalVisible(true);
-  };
+	const _renderItem = ({ item, index }) => {
+		console.log('adfjk', item)
+		const image = { uri: item.plant.photo }
+		return (
+			<View
+				style={{
+					flex: 1,
+					resizeMode: 'cover',
+					justifyContent: 'center',
+					borderRadius: 11,
+					height: 200,
+					width: 200,
+				}}>
+				<ImageBackground
+					imageStyle={styles.cardBackground}
+					source={image}
+					resizeMode='cover'
+					style={styles.cardBackground}>
+					<View style={styles.titleContainer}>
+						<Text style={styles.plantName}>
+							{item.plant.plant_type.toLowerCase()}
+						</Text>
+					</View>
+					<View style={styles.learnMore}>
+						<Pressable
+							// style={[styles.button, styles.buttonOpen]}
+							onPress={() => showPlantModal(item)}>
+							<Text style={styles.textStyle}>learn more!</Text>
+						</Pressable>
+					</View>
+				</ImageBackground>
+			</View>
+		)
+	}
 
-
-
-  const _renderItem = ({ item, index }) => {
-    const image = { uri: item.plant.photo };
-    return (
-      <View
-        style={{
-          flex: 1,
-          resizeMode: "cover",
-          justifyContent: "center",
-          borderRadius: 11,
-          height: 200,
-          width: 200,
-        }}
-      >
-        <ImageBackground
-          imageStyle={styles.cardBackground}
-          source={image}
-          resizeMode="cover"
-          style={styles.cardBackground}
-        >
-          <View style={styles.titleContainer}>
-            <Text style={styles.plantName}>{item.plant.plant_type.toLowerCase()}</Text>
-          </View>
-		  <View style={styles.learnMore}>
-		  <Pressable
-            // style={[styles.button, styles.buttonOpen]}
-            onPress={() => showPlantModal(item)}
-          >
-            <Text style={styles.textStyle}>learn more!</Text>
-          </Pressable>
-		  </View>
-          
-        </ImageBackground>
-      </View>
-    );
-  };
-
-  return (
-    <SafeAreaView
-      style={{
-        flex: 4,
-        backgroundColor: "#57784E",
-        height: 100,
-        width: 375,
-      }}
-    >
-      <View
-        style={{ flex: 0.5, flexDirection: "row", justifyContent: "center" }}
-      >
-        <Carousel
-          layout={"default"}
-          ref={(ref) => (carousel = ref)}
-          data={plants}
-          sliderWidth={100}
-          itemWidth={200}
-          padding={0}
-          renderItem={_renderItem}
-          onSnapToItem={(index) => setActiveIndex(index)}
-        />
-      </View>
-      <View
-        style={{ flex: 0.5, flexDirection: "row", justifyContent: "center" }}
-      >
-        <Carousel
-          layout={"default"}
-          ref={(ref) => (carousel = ref)}
-          data={seeds}
-          sliderWidth={100}
-          itemWidth={200}
-          renderItem={_renderItem}
-          onSnapToItem={(index) => setActiveIndex(index)}
-        />
-      </View>
-      <View
-        style={{ flex: 0.4, flexDirection: "row", justifyContent: "center" }}
-      >
-        <Carousel
-          layout={"default"}
-          ref={(ref) => (carousel = ref)}
-          data={clippings}
-          sliderWidth={100}
-          itemWidth={200}
-          renderItem={_renderItem}
-          onSnapToItem={(index) => setActiveIndex(index)}
-        />
-      </View>
-    </SafeAreaView>
-  );
-};
+	return (
+		<SafeAreaView
+			style={{
+				flex: 4,
+				backgroundColor: '#57784E',
+				height: 100,
+				width: 375,
+			}}>
+			<View
+				style={{ flex: 0.5, flexDirection: 'row', justifyContent: 'center' }}>
+				<Carousel
+					layout={'default'}
+					ref={(ref) => (carousel = ref)}
+					data={plants}
+					sliderWidth={100}
+					itemWidth={200}
+					padding={0}
+					renderItem={_renderItem}
+					onSnapToItem={(index) => setActiveIndex(index)}
+				/>
+			</View>
+			<View
+				style={{ flex: 0.5, flexDirection: 'row', justifyContent: 'center' }}>
+				<Carousel
+					layout={'default'}
+					ref={(ref) => (carousel = ref)}
+					data={seeds}
+					sliderWidth={100}
+					itemWidth={200}
+					renderItem={_renderItem}
+					onSnapToItem={(index) => setActiveIndex(index)}
+				/>
+			</View>
+			<View
+				style={{ flex: 0.4, flexDirection: 'row', justifyContent: 'center' }}>
+				<Carousel
+					layout={'default'}
+					ref={(ref) => (carousel = ref)}
+					data={clippings}
+					sliderWidth={100}
+					itemWidth={200}
+					renderItem={_renderItem}
+					onSnapToItem={(index) => setActiveIndex(index)}
+				/>
+			</View>
+		</SafeAreaView>
+	)
+}
 
 const styles = StyleSheet.create({
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  imageStyle: {
-    height: 200,
-  },
-  cardBackground: {
-    width: 200,
-    flex: 1,
-    justifyContent: "center",
-    height: 200,
-    // borderRadius: 6,
-  },
-  titleContainer: {
-    backgroundColor: "#fff4f570",
-  },
-  learnMore: {
-    backgroundColor: "#2d2d2d50",
-	width: 95,
-	marginLeft: 10,
-	padding: 1,
-  },
-  plantName: {
-    fontSize: 20,
-    fontFamily: "AvenirNext-Regular",
-    marginLeft: 10,
-    fontWeight: "bold",
-    color: "#2d2d2d",
-  },
-});
+	textStyle: {
+		color: 'white',
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	imageStyle: {
+		height: 200,
+	},
+	cardBackground: {
+		width: 200,
+		flex: 1,
+		justifyContent: 'center',
+		height: 200,
+		// borderRadius: 6,
+	},
+	titleContainer: {
+		backgroundColor: '#fff4f570',
+	},
+	learnMore: {
+		backgroundColor: '#2d2d2d50',
+		width: 95,
+		marginLeft: 10,
+		padding: 1,
+	},
+	plantName: {
+		fontSize: 20,
+		fontFamily: 'AvenirNext-Regular',
+		marginLeft: 10,
+		fontWeight: 'bold',
+		color: '#2d2d2d',
+	},
+})
 
-export default MyCarousel;
+export default MyCarousel
