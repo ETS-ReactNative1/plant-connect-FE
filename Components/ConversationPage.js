7350bec3ec0fde24474ebfa53b3eb2e14e87c67a
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-05-24 10:19:00 -0600

    Add conversation functionality

--- Components/ConversationPage.js
+++ Components/ConversationPage.js
@@ -5,4 +5,35 @@ import { createConsumer } from "@rails/actioncable";
 function ConversationPage({ loggedInUser }) {
   const [messages, setMessages] = useState([])
   const params = useParams()
+  const cable = useRef()
+
+  useEffect(() => {
+    if (!cable.current) {
+      cable.current = createConsumer("ws://localhost:3000/cable")
+    }
+    
+    const paramsToSend = {
+      channel: "ConversationChannel",
+      id: params.id
+    }
+
+    const handlers = {
+      received(data) {
+        setMessages([...messages, data])
+      },
+      connected() {
+        console.log("connected")
+      },
+      disconnected() {
+        console.log("disconnected")
+        cable.current = null
+      }
+    }
+    const subscription = cable.subscriptions.create(paramsToSend, handlers)
+    return function cleanup() {
+      console.log("unsubbing from ", params.id);
+      cable.current = null
+      subscription.unsubscribe()
+    }
+  }, [params.id, messages])
 }
